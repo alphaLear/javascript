b79842ca13e89bdde9057def6152d955b6f44d3d
yagi_ <yagitoshiro@gmail.com>
2014-12-01 01:06:40 +0900

    replace function

--- titanium/pubnub.js
+++ titanium/pubnub.js
@@ -122,11 +122,12 @@ function timeout( fun, wait ) {
  * var my_uuid = uuid();
  */
 function uuid(callback) {
-    var u = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
-    function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-    });
+    //var u = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
+    //function(c) {
+    //    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    //    return v.toString(16);
+    //});
+    var u = Ti.Platform.createUUID();
     if (callback) callback(u);
     return u;
 }
